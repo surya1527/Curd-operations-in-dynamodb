1ae40eb723ffa47d94c0846370e246d799dd83e0
Suryavamshi Sirigadhi <58841159+surya1527@users.noreply.github.com>
2022-01-23 00:55:44 +0530

    Add files via upload

---
+++ files.py
@@ -0,0 +1,37 @@
+import boto3
+
+
+def create_file_table(dynamodb=None):
+    if not dynamodb:
+        dynamodb = boto3.resource('dynamodb',region_name='ap-south-1')
+
+    table = dynamodb.create_table(
+        TableName='files',
+        KeySchema=[
+            {
+                'AttributeName': 'user_id',
+                'KeyType': 'HASH'  # Partition key
+            }
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'user_id',
+                'AttributeType': 'N'
+            }
+
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 5,
+            'WriteCapacityUnits': 5
+        }
+    )
+    return table
+
+
+
+
+
+
+if __name__ == '__main__':
+    file_table = create_file_table()
+    print("Table status:", file_table.table_status)
