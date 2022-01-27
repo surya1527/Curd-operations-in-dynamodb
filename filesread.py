5a9d5316f2ea12ecd0d2b272314ba1a68e7d7bab
Suryavamshi Sirigadhi <58841159+surya1527@users.noreply.github.com>
2022-01-27 22:10:56 +0530

    Add files via upload

---
+++ filesread.py
@@ -0,0 +1,25 @@
+from pprint import pprint
+import boto3
+from botocore.exceptions import ClientError
+
+from cats import TableName
+
+
+def get_file(dynamodb=None):
+    if not dynamodb:
+        dynamodb = boto3.resource('dynamodb',region_name='ap-south-1')
+    TableName = 'files'
+    table = dynamodb.Table(TableName)
+
+    try:
+        response = table.scan()
+        
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    else:
+        return response['Item']
+
+
+    
+
+
